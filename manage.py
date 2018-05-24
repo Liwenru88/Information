a9e2814878261a62b95d960af3ea62863c99fabc
LiWenRu <17600877300@163.com>
2018-05-24 19:32:29 +0800

    mysql数据库迁移

--- manage.py
+++ manage.py
@@ -1,7 +1,15 @@
-from info import create_app
+from info import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 # 创建app 传参给create_app函数
-app = create_app('pro')
+app = create_app('dev')
+
+# 创建脚本管理器对象
+manager = Manager(app)
+
+# 将迁移数据库脚本命令添加到manager
+manager.add_command('mysql', MigrateCommand)
 
 
 @app.route("/")
@@ -14,4 +22,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
