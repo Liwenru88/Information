81ee71b2079bedbd1543b94a4c846d17fdee7640
LiWenRu <17600877300@163.com>
2018-05-24 16:28:23 +0800

    配置MySQL数据库连接

--- manage.py
+++ manage.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """配置文件类"""
+
+    # 开启调试模式
+    DEBUG = True
+
+    # 配置mysql数据库连接
+    SQLALCHEMY_DATABASE_URI = "msyql://root:123@192.168.76.140:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
